4690dd20a9098d9261abcfae63ac74b1979674e1
IbanLee <leeyun.iban@gmail.com>
2024-06-02 12:02:24 +0800

    Update views.py

--- store/views.py
+++ store/views.py
@@ -17,7 +17,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Product, Cart, CartItem, Order, OrderItem
 
-<!-- @login_required --> #會檢測使用者是否已登入，若已登入才會執行add_to_cart()的程式
 @login_required(login_url='/admin/')
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
@@ -27,14 +26,12 @@ def add_to_cart(request, product_id):
     cart_item.save()
     return redirect('product_list')
 
-<!-- @login_required -->
 @login_required(login_url='/admin/')
 def view_cart(request):
     cart = Cart.objects.get(user=request.user)
     total_price = sum(item.product.price * item.quantity for item in cart.cartitem_set.all())
     return render(request, 'store/view_cart.html', {'cart':cart, 'total_price': total_price})
 
-<!-- @login_required -->
 @login_required(login_url='/admin/')
 def checkout(request):
     cart = Cart.objects.get(user=request.user)
